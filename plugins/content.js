96d0d602d52932d6a685b21f94d9ef6863904a3e
liangmiQwQ <mi_yyds@126.com>
2025-02-22 16:42:37 +0800

    feat: add prettier

--- plugins/content.js
+++
@@ -1,5 +0,0 @@
-// 获取网页中的所有文本内容
-const pageText = document.body.innerText;
-
-// 打印到控制台
-console.log("Page Text Content:", pageText);
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  /*build: {
+    rollupOptions: {
+      input: {
+        popup: 'index.html',
+        manifest: 'manifest.json' // 将 manifest 文件打包
+      },
+    },
+  },*/
 })
